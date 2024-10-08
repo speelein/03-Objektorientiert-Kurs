81610f23dccaba97159e90faf118670ceab155bd
speelein <pruefer.g@gmx.de>
2024-10-08 10:19:50 +0200

    erster Commit

---
+++ Klassen/03-Funktion exter auserhalb der class.py
@@ -0,0 +1,21 @@
+# in der Klasse wurden auch funktionen definiert. Aber nicht empohlen ausser 
+
+class Archer:
+    hp = 100
+    mana = 0
+    # es wird jetzt diese Funktion eingefuegt
+    # printet aber nur eigenartig den Text ich bin.....
+    def walk(self):
+        print(f'ich bin {self}und laufe')
+
+print(Archer.hp)
+print(Archer.__dict__)
+print(type(Archer.__dict__))
+
+# Instanz von Archer erzeuegen
+archer1 = Archer()
+print(archer1.__dict__)
+# { } dict ist leer keine Werte enthalten
+
+archer1.walk()
+# Es wird in archer1 trozdem die Funktion ausgegebne, offensichtlich an archer1 vererbt
