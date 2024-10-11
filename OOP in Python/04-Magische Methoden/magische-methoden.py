0683772c180574c8cd81a65cd86a126f9ad147ec
speelein <pruefer.g@gmx.de>
2024-10-11 08:31:11 +0200

    bis OOP inPython erledigt

--- OOP in Python/04-Magische Methoden/magische-methoden.py
+++ OOP in Python/04-Magische Methoden/magische-methoden.py
@@ -5,16 +5,16 @@ class Buch:
         self.autor = autor
         self.anzahl_seiten = anzahl_seiten
 
-    def uber_tausend_seiten(self):
+    def ueber_tausend_seiten(self):
         return self.anzahl_seiten > 1000
     
     def __len__(self): # magig method fuer len, dann kann unten aufgerufen werden
-        return print(self.anzahl_seiten)
+        return self.anzahl_seiten
     
     def __str__(self):
         return self.titel
     
-    def __add__(self, other):
+    def __add__(self, other):  # muss nicht mit other, XXX moeglich, nur fuer Zweiten Wert steht other oder ..... XXX
         return self.anzahl_seiten + other.anzahl_seiten # Opperatoroverload
     
 titel_1 = "Harry Potter und der Feuerkelch"
@@ -23,14 +23,14 @@ autor  = "J.K.Rowling"
 
 feuerkelch = Buch(titel_1, autor, 767)
 halbblutprinz = Buch(titel_2, autor, 656)
-
-#len(feuerkelch)
+print(feuerkelch.ueber_tausend_seiten())  # erst das Objekt, dann die Funktion, def ueber_tausend_seiten
+print(len(feuerkelch)) # __len__ aufruf
+print(feuerkelch.__len__())
 print(str(halbblutprinz))
-
-#print(__len__(feuerkelch))
-#print(__str__(feuerkelch))
-
-print(feuerkelch + halbblutprinz)
+print(titel_2.__str__()) # so oder
+print(str(feuerkelch))  # auch so
+print(feuerkelch) # str automatisch
+print(feuerkelch + halbblutprinz) # siehe def __add__
 
 """
 5, "strings", listen..... werden in Python als Objekte behandelt.
---
+++ OOP in Python/04-Magische Methoden/oblekte_in_python.py
@@ -0,0 +1,16 @@
+print(9+3)
+print("ab"+ "cd")
+print([1, 2] + [3, 4])
+
+# Der plusopperator hat jeweils eine ander Bedeutung
+
+print((9).__add__ (3))
+print("ab".__add__("cd"))
+print([1, 2].__add__([3, 4]))
+
+"""
+Es sind Objekte
+
+
+"""
+
