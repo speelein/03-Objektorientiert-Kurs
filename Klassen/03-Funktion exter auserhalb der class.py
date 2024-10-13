b9d17b8c21836ecb77b68eb0e33fc1ef2b26006f
speelein <pruefer.g@gmx.de>
2024-10-13 10:19:34 +0200

    Ordner Klassen aufgeraeumt

--- Klassen/03-Funktion exter auserhalb der class.py
+++ Klassen/03-Funktion exter auserhalb der class.py
@@ -7,7 +7,7 @@ class Archer:
     # printet aber nur eigenartig den Text ich bin.....
     def walk(self):
         print(f'ich bin {self} und laufe')
-    
+hp+20
 print(Archer.mana)
 print(Archer.__dict__)
 print(type(Archer.__dict__))
@@ -15,7 +15,7 @@ print(type(Archer.__dict__))
 # Instanz von Archer erzeuegen
 archer1 = Archer() # warum leer {}
 archer2 = Archer()
-print(archer1.__dict__)
+print(archer1.mana)
 print(archer2.__dict__)
 # { } dict ist leer keine Werte enthalten
 
--- OOP in Python/04-Magische Methoden/objekte_in_python.py
+++ OOP in Python/04-Magische Methoden/objekte_in_python.py
@@ -8,8 +8,15 @@ print((9).__add__ (3))
 print("ab".__add__("cd"))
 print([1, 2].__add__([3, 4]))
 
+
+print(type((9).__add__ (3)))
+print(type("ab".__add__("cd")))
+print(type([1, 2].__add__([3, 4])))
+
 """
-Es sind Objekte
+Es sind Objekte. 
+die __add__ erkennt also den Typ 
+automatisch, so programmiert in Python
 
 
 """
